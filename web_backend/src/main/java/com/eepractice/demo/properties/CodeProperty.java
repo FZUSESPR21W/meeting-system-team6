9dabb513152d6a8f6a8f2c201544490e05a8932b
YangZX1428 <60736007+YangZX1428@users.noreply.github.com>
2021-03-27 21:35:49 +0800

    完成辅助类工具和常量类的定义

---
+++ web_backend/src/main/java/com/eepractice/demo/properties/CodeProperty.java
@@ -0,0 +1,13 @@
+package com.eepractice.demo.properties;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CodeProperty {
+    public final int PARAMETER_ERROR = 201;
+    public  final int LOGIN_FAILED = 202;
+    public final int SUCCESS = 200;
+    public final int TOKEN_INVALID = 203;
+    public final int NO_PERMISSIONS = 204;
+    public final int REGISTER_FAILED = 205;
+}
